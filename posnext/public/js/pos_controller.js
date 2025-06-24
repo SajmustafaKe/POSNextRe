660ea346a2dbf16774ce8f68e661474f96a7c629
KasuniB <113013082+KasuniB@users.noreply.github.com>
2025-06-24 07:55:37 +0300

    changes

--- posnext/public/js/pos_controller.js
+++ posnext/public/js/pos_controller.js
@@ -727,79 +727,78 @@ make_new_invoice(from_held = false) {
 		this.page.set_indicator(this.pos_profile, "blue");
 	}
 
-	async on_cart_update(args) {
-		frappe.dom.freeze();
-		let item_row = undefined;
-		try {
-			let { field, value, item } = args;
-			item_row = this.get_item_from_frm(item);
-			const item_row_exists = !$.isEmptyObject(item_row);
+async on_cart_update(args) {
+    // REMOVE THIS LINE: frappe.dom.freeze();
+    let item_row = undefined;
+    try {
+        let { field, value, item } = args;
+        item_row = this.get_item_from_frm(item);
+        const item_row_exists = !$.isEmptyObject(item_row);
 
-			const from_selector = field === 'qty' && value === "+1";
-			if (from_selector)
-				value = flt(item_row.stock_qty) + flt(value);
+        const from_selector = field === 'qty' && value === "+1";
+        if (from_selector)
+            value = flt(item_row.stock_qty) + flt(value);
 
-			if (item_row_exists) {
-				if (field === 'qty')
-					value = flt(value);
+        if (item_row_exists) {
+            if (field === 'qty')
+                value = flt(value);
 
-				if (['qty', 'conversion_factor'].includes(field) && value > 0 && !this.allow_negative_stock) {
-					const qty_needed = field === 'qty' ? value * item_row.conversion_factor : item_row.qty * value;
-					await this.check_stock_availability(item_row, qty_needed, this.frm.doc.set_warehouse);
-				}
+            if (['qty', 'conversion_factor'].includes(field) && value > 0 && !this.allow_negative_stock) {
+                const qty_needed = field === 'qty' ? value * item_row.conversion_factor : item_row.qty * value;
+                await this.check_stock_availability(item_row, qty_needed, this.frm.doc.set_warehouse);
+            }
 
-				if (this.is_current_item_being_edited(item_row) || from_selector) {
-					await frappe.model.set_value(item_row.doctype, item_row.name, field, value);
-					this.update_cart_html(item_row);
-				}
+            if (this.is_current_item_being_edited(item_row) || from_selector) {
+                await frappe.model.set_value(item_row.doctype, item_row.name, field, value);
+                this.update_cart_html(item_row);
+            }
 
-			} else {
-				if (!this.frm.doc.customer && !this.settings.custom_mobile_number_based_customer){
-					return this.raise_customer_selection_alert();
-				}
-				frappe.flags.ignore_company_party_validation = true
-				const { item_code, batch_no, serial_no, rate, uom } = item;
+        } else {
+            if (!this.frm.doc.customer && !this.settings.custom_mobile_number_based_customer){
+                return this.raise_customer_selection_alert();
+            }
+            frappe.flags.ignore_company_party_validation = true
+            const { item_code, batch_no, serial_no, rate, uom } = item;
 
-				if (!item_code)
-					return;
+            if (!item_code)
+                return;
 
-				const new_item = { item_code, batch_no, rate, uom, [field]: value };
-				if(value){
-					new_item['qty'] = value
-				}
-				if (serial_no) {
-					await this.check_serial_no_availablilty(item_code, this.frm.doc.set_warehouse, serial_no);
-					new_item['serial_no'] = serial_no;
-				}
+            const new_item = { item_code, batch_no, rate, uom, [field]: value };
+            if(value){
+                new_item['qty'] = value
+            }
+            if (serial_no) {
+                await this.check_serial_no_availablilty(item_code, this.frm.doc.set_warehouse, serial_no);
+                new_item['serial_no'] = serial_no;
+            }
 
-				if (field === 'serial_no')
-					new_item['qty'] = value.split(`\n`).length || 0;
+            if (field === 'serial_no')
+                new_item['qty'] = value.split(`\n`).length || 0;
 
-				item_row = this.frm.add_child('items', new_item);
+            item_row = this.frm.add_child('items', new_item);
 
-				if (field === 'qty' && value !== 0 && !this.allow_negative_stock) {
-					const qty_needed = value * item_row.conversion_factor;
-					await this.check_stock_availability(item_row, qty_needed, this.frm.doc.set_warehouse);
-				}
+            if (field === 'qty' && value !== 0 && !this.allow_negative_stock) {
+                const qty_needed = value * item_row.conversion_factor;
+                await this.check_stock_availability(item_row, qty_needed, this.frm.doc.set_warehouse);
+            }
 
-				await this.trigger_new_item_events(item_row);
+            await this.trigger_new_item_events(item_row);
+            this.update_cart_html(item_row);
 
-				this.update_cart_html(item_row);
+            if (this.item_details.$component.is(':visible'))
+                this.edit_item_details_of(item_row);
 
-				if (this.item_details.$component.is(':visible'))
-					this.edit_item_details_of(item_row);
+            if (this.check_serial_batch_selection_needed(item_row) && !this.item_details.$component.is(':visible'))
+                this.edit_item_details_of(item_row);
+        }
 
-				if (this.check_serial_batch_selection_needed(item_row) && !this.item_details.$component.is(':visible'))
-					this.edit_item_details_of(item_row);
-			}
-
-		} catch (error) {
-			console.log(error);
-		} finally {
-			frappe.dom.unfreeze();
-			return item_row; // eslint-disable-line no-unsafe-finally
-		}
-	}
+    } catch (error) {
+        console.log(error);
+    } finally {
+        // REMOVE THIS LINE: frappe.dom.unfreeze();
+        return item_row;
+    }
+}
 
 	raise_customer_selection_alert() {
 		frappe.dom.unfreeze();
