62ca02aab04caf7e20ad425ede29145e0b9a7f5d
KasuniB <113013082+KasuniB@users.noreply.github.com>
2025-08-04 10:04:55 +0300

    changes

--- posnext/hooks.py
+++ posnext/hooks.py
@@ -185,8 +185,8 @@ override_doctype_class = {
 # 	"frappe.desk.doctype.event.event.get_events": "posnext.event.get_events"
 # }
 override_whitelisted_methods = {
-	"erpnext.accounts.doctype.pos_closing_entry.pos_closing_entry.get_pos_invoices": "posnext.overrides.pos_closing_entry.get_pos_invoices",
-	"erpnext.accounts.doctype.pos_invoice.pos_invoice.get_stock_availability": "posnext.overrides.pos_invoice.get_stock_availability"
+	"erpnext.accounts.doctype.pos_closing_entry.pos_closing_entry.get_pos_invoices": "posnext.overrides.pos_closing_entry.get_pos_invoices"
+	#"erpnext.accounts.doctype.pos_invoice.pos_invoice.get_stock_availability": "posnext.overrides.pos_invoice.get_stock_availability"
 }
 #
 # each overriding function accepts a `data` argument;
